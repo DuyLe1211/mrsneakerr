7055d9aeb7d1e1f2159542877c6472da6dfc2a71
DuyLee <leminhduy12112007@gmail.com>
2023-01-01 09:24:36 +0700

    fix responsive

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let product = document.querySelectorAll('.header__link')[1]
+let body = document.querySelector('.body')
 let bodyText = document.querySelector('.body__text')
 let bodyImg = document.querySelector('.body__img')
 let productsList = document.querySelector('.products-container')
@@ -7,16 +8,19 @@ function productClicked() {
     let productClass = product.classList
     let bodyImgClass = bodyImg.classList
     let productsListClass = productsList.classList
+    let bodyClass = body.classList
     if (productClass.contains('product-click')) {
         productClass.remove('product-click')
         bodyTextClass.remove('body__text--click')
         bodyImgClass.remove('body__img--click')
         productsListClass.remove('products-container--click')
+        bodyClass.remove('body--click')
     } else {
         productClass.add('product-click')
         bodyTextClass.add('body__text--click')
         bodyImgClass.add('body__img--click')
         productsListClass.add('products-container--click')
+        bodyClass.add('body--click')
     }
 }
 
